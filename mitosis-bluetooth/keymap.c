67861845353d4b58681f3dc7346658f691e4a4c7
William <wrwilson@smu.edu>
2018-08-01 21:23:38 -0500

    update linux program.sh and keymap

--- mitosis-bluetooth/keymap.c
+++ mitosis-bluetooth/keymap.c
@@ -1,49 +1,194 @@
 // this is the style you want to emulate.
 // This is the canonical layout file for the Quantum project. If you want to add another keyboard,
 
-#include "mitosis.h"
+#include "keymap.h"
+#include "/home/william/GlenPickle/qmk_firmware-1/keyboards/chimera_ortho/chimera_ortho.h"
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-enum mitosis_layers
+enum chimera_ortho_layers
 {
-  _BL,
-  _RAISE
+	_QWERTY,
+	_CAPS,
+	_NUMPAD,
+	_SYMBOLS,
+	_MACROS,
+	_NAV
 };
 
-enum mitosis_keycodes
-{
-  FNKEY = SAFE_RANGE,
-  RAISE
-};
+#define KC_NMPD TG(_NUMPAD)
+#define KC_SYMB TG(_SYMBOLS)
+#define KC_SPFN LT(_NAV,KC_EQL)
+#define KC_SCTL MT(MOD_LCTL, KC_LBRC)
+#define KC_SCTR MT(MOD_LCTL, KC_RBRC)
+#define KC_SPLT MT(MOD_LALT, KC_MINS)
+#define KC_SPRT MT(MOD_LALT, KC_1)
+#define KC_GBRC MT(MOD_RGUI, KC_8)
+#define KC_GQOT MT(MOD_LGUI, KC_QUOT)
+#define KC_MESC LT(_MACROS, KC_ESC)
+#define KC_INCL M(0)
+#define KC_PULL M(1)
+#define KC_PUSH M(2)
+#define KC_SCAP M(3)
+#define KC_SCOF M(4)
+#define KC_CAD LALT(LCTL(KC_DEL))
 
+#define LONGPRESS_DELAY 150
+//#define LAYER_TOGGLE_DELAY 300
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
-#define DUAL_R  LT(_RAISE,KC_SPC)
+#define KC_ KC_TRNS 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[_BL] = { /* test mitosis-mjt-like qwerty layout */
-  {KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,     KC_P    },
-  {KC_A,    KC_S,    KC_D,    KC_F,    KC_G,          KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN },
-  {KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH },
-  {XXXXXXX, KC_LALT, KC_ESC,  KC_TAB,  KC_QUOT,       KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, XXXXXXX },
-  {XXXXXXX, KC_LCTL, KC_LGUI, KC_SPC,  KC_LSFT,       DUAL_R,  KC_BSPC, KC_ENT,  KC_RGUI,  XXXXXXX }
-},
+  [_QWERTY] = KC_KEYMAP(  
+  //,----+----+----+----+----+----+----.     ,----+----+----+----+----+----+----.
+     MESC, Q  , W  , E  , R  , T  ,SCTL,      SCTR, Y  , U  , I  , O  , P  ,QUOT,
+  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
+     TAB , A  , S  , D  , F  , G  ,SPLT,      SPRT, H  , J  , K  , L  ,SCLN,ENT ,
+  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
+     LSPO, Z  , X  , C  , V  , B  ,SPFN,      GBRC, N  , M  ,COMM,DOT ,SLSH,RSPC,
+  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
+                         NMPD,BSPC,                SPC ,SYMB
+  // \------------------+----+----+---/       \---+----+----+-------------------/
+  ),
 
+  [_CAPS] = KC_KEYMAP(  
+  //,----+----+----+----+----+----+----.     ,----+----+----+----+----+----+----.
+         ,    ,    ,    ,    ,    ,    ,          ,    ,    ,    ,    ,    ,    ,
+  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
+         ,    ,    ,    ,    ,    ,UNDS,          ,    ,    ,    ,    ,COLN,    ,
+  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
+     SCOF,    ,    ,    ,    ,    ,    ,          ,    ,    ,    ,    ,    ,SCOF,
+  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
+                             ,    ,                    ,    
+  // \------------------+----+----+---/       \---+----+----+-------------------/
+  ),
 
+  [_NUMPAD] = KC_KEYMAP(  
+  //,----+----+----+----+----+----+----.     ,----+----+----+----+----+----+----.
+         ,    ,COLN ,    ,    ,    ,    ,          ,    , 7  , 8  , 9  ,ASTR,MINS,
+  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
+         ,    ,DOT ,    ,    ,    ,    ,          ,    , 4  , 5  , 6  ,PLUS,    ,
+  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
+         ,    ,    ,    ,    ,    ,    ,          ,    , 1  , 2  , 3  ,SLSH,    ,
+  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
+                             ,    ,                     , 0  
+  // \------------------+----+----+---/       \---+----+----+-------------------/
+  ),
 
-[_RAISE] = {
-  {KC_1,    KC_2,    KC_3,   KC_4,     KC_5,          KC_6,    KC_7,    KC_8,    KC_9,     KC_0    },
-  {KC_F1,   KC_F2,   KC_F3,  KC_F4,    KC_F5,         KC_F6,   KC_MINS, KC_EQL,  KC_LBRC,  KC_RBRC },
-  {KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_F11,        KC_F12,  KC_BSLS, _______, _______,  KC_QUOT },
-  {XXXXXXX, _______, KC_BRK, KC_CAPS,  KC_GRV,        KC_HOME, KC_PGDN, KC_PGUP, KC_END,   XXXXXXX },
-  {XXXXXXX, _______, _______, _______, _______,       _______, KC_DEL,  KC_PSCR, KC_MENU,  XXXXXXX }
-}
+  [_SYMBOLS] = KC_KEYMAP(  
+  //,----+----+----+----+----+----+----.     ,----+----+----+----+----+----+----.
+         ,EXLM, AT ,HASH,DLR ,PERC,    ,          ,CIRC,AMPR,ASTR,LPRN,RPRN,BSLS,
+  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
+         , F1 , F2 , F3 , F4 , F5 ,    ,          ,TILD,COLN,UNDS,LCBR,RCBR,    ,
+  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
+         , F6 , F7 , F8 , F9 ,F10 ,    ,          ,GRV ,SCLN,MINS,LBRC,RBRC,    ,
+  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
+                         PIPE,    ,                    ,
+  // \------------------+----+----+---/       \---+----+----+-------------------/
+  ),
+
+  [_NAV] = KC_KEYMAP(  
+  //,----+----+----+----+----+----+----.     ,----+----+----+----+----+----+----.
+         ,    ,    ,    ,    ,    ,    ,          ,    ,    , UP ,    ,PSCR,    ,
+  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
+         ,    ,    ,    ,    ,    ,    ,          ,    ,LEFT,DOWN,RGHT,    ,    ,
+  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
+         ,    ,    ,    ,    ,    ,    ,          ,    ,PGUP,PGDN,    ,    ,    ,
+  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
+                             ,DEL ,                    ,    
+  // \------------------+----+----+---/       \---+----+----+-------------------/
+  ),
+
+  [_MACROS] = KC_KEYMAP(  
+  //,----+----+----+----+----+----+----.     ,----+----+----+----+----+----+----.
+         ,    ,    ,    ,    ,    ,    ,          ,    ,    ,INCL,    ,    ,    ,
+  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
+         ,    ,    ,CAD ,    ,    ,    ,          ,    ,    ,    ,    ,    ,    ,
+  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
+     SCAP,    ,    ,    ,    ,    ,    ,          ,    ,    ,PULL,PUSH,    ,SCAP,
+  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
+                             ,    ,                    ,    
+  // \------------------+----+----+---/       \---+----+----+-------------------/
+  )
 
 };
 
+
+const uint16_t PROGMEM fn_actions[] = {
+
+};
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+  switch(id) {
+    /* include some kind of library or header */
+    case 0:
+      if (record->event.pressed) {
+        SEND_STRING("#include <>");
+        return MACRO( T(LEFT), END);
+      }
+      break;
+    case 1:
+      if (record->event.pressed) {
+        SEND_STRING("git pull");
+        return MACRO( T(ENT), END );
+      }
+      break;
+    case 2:
+      if (record->event.pressed){
+        SEND_STRING("git push");
+        return MACRO( T(ENT), END );
+      }
+      break;
+    case 3:
+      if (record->event.pressed){
+        layer_on(_CAPS);
+        register_code(KC_CAPSLOCK);
+        unregister_code(KC_CAPSLOCK);
+      }
+      break;
+    case 4:
+      if (record->event.pressed){
+        layer_off(_CAPS);
+        register_code(KC_CAPSLOCK);
+        unregister_code(KC_CAPSLOCK);
+      }
+      break;
+  }
+  return MACRO_NONE;
+};
+ 
+
+void matrix_scan_user(void) {
+    uint8_t layer = biton32(layer_state);
+    
+    switch (layer) {
+    	case _QWERTY:
+    	    set_led_green;
+    	    break;
+        case _CAPS:
+	    set_led_white;
+	    break;
+        case _NUMPAD:
+            set_led_blue;
+            break;
+        case _SYMBOLS:
+            set_led_red;
+            break;
+        case _NAV:
+	    set_led_magenta;
+	    break;
+        case _MACROS:
+	    set_led_cyan;
+	    break;
+       default:
+            set_led_green;
+            break;
+    }
+};
--- mitosis-bluetooth/program.sh
+++ mitosis-bluetooth/program.sh
@@ -6,6 +6,8 @@ if [[ $? -ne 0 ]] ; then
     exit 0
 fi
 sleep 0.1
+#SOFTHEX=`readlink -f /home/william/GlenPickle/nrf51_SDK_10/components/softdevice/s110/hex/s110_nrf51_8.0.0_softdevice.hex`
+SOFTHEX=`readlink -f /home/william/GlenPickle/nRF5_SDK_11/components/softdevice/s130/hex/s130_nrf51_2.0.0_softdevice.hex`
 HEX=`readlink -f _build/nrf51822_xxac.hex`
 du -b $HEX
 
@@ -14,8 +16,21 @@ echo '============================= PROGRAMMING ============================='
 {
 	echo "reset halt";
 	sleep 0.1;
+	echo "flash write_image erase" $SOFTHEX;
+	sleep 30;
+	echo "reset";
+	sleep 0.1;
+	exit;
+
+} | telnet localhost 4444
+
+echo
+echo '============================= PROGRAMMING ============================='
+{
+	echo "reset halt";
+	sleep 0.1;
 	echo "flash write_image erase" $HEX;
-	sleep 11;
+	sleep 15;
 	echo "reset";
 	sleep 0.1;
 	exit;
